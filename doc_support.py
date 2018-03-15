2b08c3759182419f954b125019fd86d27f484fe3
insiyaa <insiyahhajoori@gmail.com>
2018-03-15 23:34:49 +0530

    Combine two similar sections

--- doc_support.py
+++ doc_support.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
-
-
+    
 def parseHTML(url):
     client = urlopen(url)
     page_html = client.read()
@@ -10,7 +9,7 @@ def parseHTML(url):
     page_soup = soup(page_html,"html.parser")
     return page_soup
 
-def getnavList(page_soup):
+def getNavList(page_soup):
     index = page_soup.find_all("div", {"class":"toctree-wrapper"})
     index_items = index[0].find_all("a", {"reference internal"})
     return index_items
@@ -19,7 +18,7 @@ def printNavList(index_items):
     for item in index_items:
         print(item.text)
 
-def printDocdata(nav, nav_url, index_items):
+def printDocData(nav, nav_url, index_items):
     nav = nav.rstrip('.')
     k = len(nav)
     for item in index_items:
@@ -32,8 +31,9 @@ def printDocdata(nav, nav_url, index_items):
             page_soup = parseHTML(nav_url + item['href'])
             req_div = page_soup.find_all("div",{"id":div_id})
             print(req_div[0].text)
-            return
+            return True
     print("Invalid choice!")
+    return False
 
 
 def main():
@@ -45,60 +45,47 @@ def main():
             version = int(input())
         except:
             print("Invalid choice: Enter 1 or 2")
-        if version == 1:
-            while 1:
-                nav_url = "https://docs.python.org/3/reference/"
-                my_url = "https://docs.python.org/3/reference/index.html"
-                page_soup = parseHTML(my_url)
-                index_items = getnavList(page_soup)
-
-                printNavList(index_items)
-                print("Enter:")
-                nav = input()
-                if nav == "q":
-                    flag = 0
-                    break
-                printDocdata(nav, nav_url, index_items)
-                print("q: quit\nl: documentation menu\nc: change python version")
-                choice = input()
-                if choice == "q":
-                    flag = 0
-                    break
-                elif choice == "l":
-                    continue
-                elif choice == "c":
-                    flag = 1
-                    break
-
-        elif version == 2:
-            while 1:
-                nav_url = "https://docs.python.org/2.7/reference/"
-                my_url = "https://docs.python.org/2.7/reference/index.html"
-                page_soup = parseHTML(my_url)
-                index_items = getnavList(page_soup)
-
-                printNavList(index_items)
-                print("Enter:")
-                nav = input()
-                if nav == "q":
-                    flag = 0
-                    break
-                printDocdata(nav, nav_url, index_items)
-                print("q: quit\nl: documentation menu\nc: change python version")
-                choice = input()
-                if choice == "q":
-                    flag = 0
-                    break
-                elif choice == "l":
-                    continue
-                elif choice == "c":
-                    flag = 1
-                    break
-        else:
-            print("Invalid choice")
             continue
         
+        if version not in [1,2]:
+            print("Invalid choice, try again.")
+            continue
+        else:
+            if version == 1:
+                placeholder = 3
+            else:
+                placeholder = 2.7
+            while 1:
+                nav_url = "https://docs.python.org/{}/reference/".format(placeholder)
+                my_url = "https://docs.python.org/{}/reference/index.html".format(placeholder)
+                page_soup = parseHTML(my_url)
+                index_items = getNavList(page_soup)
+
+                printNavList(index_items)
+                print("Enter:")
+                nav = input()
+                if nav == "q":
+                    flag = 0
+                    break
+                elif nav == "c":
+                    flag = 1
+                    break
+                if not printDocData(nav, nav_url, index_items):
+                    continue
+                print("q: quit\nl: documentation menu\nc: change python version")
+                choice = input()
+                if choice == "q":
+                    flag = 0
+                    break
+                elif choice == "l":
+                    continue
+                elif choice == "c":
+                    flag = 1
+                    break
+                else:
+                    print("Invalid choice.")
         if flag == 0:
+            flag = 1
             break
 
             
